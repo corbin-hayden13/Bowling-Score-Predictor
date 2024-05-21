e20d6c0d308cbe3b0accbe335412e996d4bff885
corbin-hayden13 <haydenfcorbin@gmail.com>
2024-05-20 21:04:04 -0500

    Getting the UI setup, making some table design choices

---
+++ src/ui-components/table/GameTable.js
@@ -0,0 +1,24 @@
+export default function GameTable({framesOneToNine, frameTen}) {
+    return (
+        <table className="bowling-table">
+            <thead>
+                <tr>
+                    <th>Frame 1</th>
+                    <th>Frame 2</th>
+                    <th>Frame 3</th>
+                    <th>Frame 4</th>
+                    <th>Frame 5</th>
+                    <th>Frame 6</th>
+                    <th>Frame 7</th>
+                    <th>Frame 8</th>
+                    <th>Frame 9</th>
+                    <th>Frame 10</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
---
+++ src/ui-components/table/TableCell.js
@@ -0,0 +1,27 @@
+import "./Bowling-Table.css";
+
+export default function TableCell({frameNum, scores, currScore, isSelected, clickFunc}) {
+    const firstThrow = scores[0] !== undefined && scores[0] >= 0 ? `${scores[0]}` : " ";
+    const secondThrow = scores[1] !== undefined && scores[1] >= 0 ? `${scores[1]}` : " ";
+    const thirdThrow = (frameNum === 10) && scores[2] !== undefined && scores[2] >= 0 ? `${scores[2]}` : " ";
+
+    const doubleCell = (
+        <div className={`table-cell ${isSelected ? 'selected' : ''}`} onClick={clickFunc}>
+            <div className="top-left">{firstThrow}</div>
+            <div className="top-right">{secondThrow}</div>
+            <div className="bottom-double">{currScore}</div>
+        </div>
+    );
+
+    const tripleCell = (
+        <div className={`table-cell ${isSelected ? 'selected' : ''}`} onClick={clickFunc}>
+            <div className="top-left">{firstThrow}</div>
+            <div className="top-right">{secondThrow}</div>
+            <div className="top-right">{thirdThrow}</div>
+            <div className="bottom-triple">{currScore}</div>
+        </div>
+    );
+
+    if (frameNum !== 10) return doubleCell;
+    else return tripleCell;
+}
