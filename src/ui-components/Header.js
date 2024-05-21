2a70e6f3237d4703a012f3443f3c0b786007d407
corbin-hayden13 <haydenfcorbin@gmail.com>
2024-05-21 16:57:09 -0500

    Frames update throws, not current score

--- src/ui-components/Header.js
+++ src/ui-components/Header.js
@@ -1,10 +1,12 @@
 import { useGlobals } from "../Globals";
 
 export default function Header() {
-    const { games, addGame } = useGlobals();
+    const { games, addGame, setFrame } = useGlobals();
 
     const handleOnClick = () => {
         addGame(0, "Test Game");
+        setFrame(0, 3, [0, 7]);
+        setFrame(0, 10, [7, 3, 0]);
     };
 
     return (
